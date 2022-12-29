b045c07bf498f91b35c8c4ef4d76ca6bf52caa74
Robert Buskirk-Lechner <buskirklechner@gmail.com>
2022-12-29 09:19:03 -0500

    added buttons to change item quantitiy, still having issues with quantity values rerendering in real time

--- pakrat_client/src/components/pak_components/Item.jsx
+++ pakrat_client/src/components/pak_components/Item.jsx
@@ -1,10 +1,31 @@
+import { useContext, useEffect } from 'react'
+import { DataContext } from '../../DataContext'
+
 export default function Item ({itemInfo, itemIndex}) {
-    console.log(itemInfo)
+
+    const { pakItems, setPakItems } = useContext(DataContext)
+
+
+    const add = (event) => {
+        event.preventDefault()
+        pakItems[itemIndex].quantity += 1
+    }
+    const sub = (event) => {
+        event.preventDefault()
+        if (pakItems[itemIndex].quantity > 0) {
+            pakItems[itemIndex].quantity -= 1
+        }
+    }
+
+
+
     return (
         <div>
             <div>{itemInfo.name}</div>
+            <div>{pakItems[itemIndex].quantity}</div>
             <div>
-                
+                <button onClick={add}>Add</button>
+                <button onClick={sub}>Sub</button>
             </div>
         </div>
     )
