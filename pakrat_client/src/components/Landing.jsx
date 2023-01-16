4c885508173b7ed0314e6ffe7999a031241d9375
Robert Buskirk-Lechner <buskirklechner@gmail.com>
2023-01-16 08:20:07 -0500

    landing page update

--- pakrat_client/src/components/Landing.jsx
+++ pakrat_client/src/components/Landing.jsx
@@ -13,17 +13,17 @@ export default function Landing () {
         navigate(`${path}${param ? param : ''}`)
     }
 
-    const buttonStyle = `bg-med w-24 h-8 flex items-center justify-center rounded-md cursor-pointer my-4 hover:bg-dark hover:text-light duration-300`
+    const buttonStyle = `bg-med w-24 mx-auto h-8 flex items-center justify-center rounded-md cursor-pointer my-4 hover:bg-dark hover:text-light duration-300`
 
     return (
         <div className="grid grid-cols-2 m-10">
-            <div className="mx-auto">
+            <div className="m-auto">
+                <div className="mx-auto text-center">Welcome to PakRat. Try out the 'Pak' packing list form here. Register and Log In to save an reuse Paks.</div>
+                <UlPak/>
+            </div>
+            <div className="mx-auto w-3/5 border-2 rounded-lg border-highlight m-auto">
                 <div onClick={() => goTo('/login')} className={buttonStyle}>Log In</div>
                 <div onClick={() => goTo('/register')} className={buttonStyle}>Register</div>
-            </div>
-            <div className="m-auto">
-            <div className="mx-auto text-center">Welcome to PakRat. Try out the 'Pak' packing list form here. Register and Log In to save an reuse Paks.</div>
-                <ULPak/>
             </div>
         </div>
     )
